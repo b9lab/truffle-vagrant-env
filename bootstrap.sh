dd7003f9c9a78433129d99a67d5b345bcdf03022
Xavier Lepretre <xavier@b9lab.com>
2017-01-04 13:07:49 +0100

    Updated versions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,22 +6,20 @@ add-apt-repository -y ppa:ethereum/ethereum
 add-apt-repository -y ppa:ethereum/ethereum-dev
 add-apt-repository -y ppa:ethereum/ethereum-qt
 curl -sL https://deb.nodesource.com/setup_iojs_2.x | sudo -E bash -
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
 apt-get update
 # apt-get upgrade -y
 
 apt-get install -y git python dstat ntp nodejs build-essential \
-	solc ethereum cpp-ethereum
+	solc ethereum 
 
 service ntp reload
 
 # For custom build config
-npm install -g truffle@2.0.4 \
+npm install -g truffle@2.1.1 \
 	truffle-default-builder ether-pudding rimraf web3 \
-	ethereumjs-testrpc@2.0.9 \
-# ethereumjs-testrpc@2.1.0 is broken
-	q
+	node-gyp ethereumjs-testrpc@3.0.3 q
 
 # To get a local Web server from your build folder: 
 # php -S 0.0.0.0:8000
